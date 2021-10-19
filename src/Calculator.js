61e0dff3bc25eadc7e2f9be7150cc0cf69fe6d44
St-Pardon <okc4pardon@gmail.com>
2021-10-19 16:57:01 +0100

    1 file modified

--- src/Calculator.js
+++ src/Calculator.js
@@ -29,14 +29,18 @@ class Calculator extends React.Component{
     });
   }
   handleNumbers(e){
-    if(this.state.displayInput === ""){
-      console.log("zero1")
+    if(this.state.evaluated) {
+      this.setState({
+        displayInput: e.target.value,
+        result: e.target.value,
+        evaluated: false
+      })
+    }else if(this.state.displayInput === ""){
       this.setState({
         displayInput: e.target.value,
         result: e.target.value
       });
     } else {
-      console.log("zero2")
       this.setState({
         displayInput: this.state.displayInput === "0" && e.target.value === "0" ? this.state.displayInput : this.state.displayInput +  e.target.value,
         result: this.state.displayInput === "0" && e.target.value === "0" ? this.state.displayInput : this.state.result +  e.target.value
@@ -46,11 +50,10 @@ class Calculator extends React.Component{
 
   handleOperators(e){
     this.setState({result: e.target.value})
-    // if(this.state.displayInput !== "" && e.target.value !== "="){
         if(this.state.evaluated){
           this.setState({
             displayInput: this.state.prevVal + e.target.value,
-            // result: "0"
+            evaluated: false
           })
         } else if (!endsWithOperator.test(this.state.displayInput)){
           this.setState({
@@ -80,54 +83,70 @@ class Calculator extends React.Component{
       //   })
       //  else if (e.target.value === "="){
       //   this.handleEqualsTo()
-    
       // } 
   }
   
   handleDecimal(e){
-    let ini = /[\+\-\x\/]\d+$/.test(this.state.displayInput);  // checks +23 
-    let regex = /(\d+\.\d+)$/g.test(this.state.displayInput); // checks 12.34
-    let des = /\.$/.test(this.state.displayInput); // match decimal
-    let signs = /[\+\-\x\/]$/.test(this.state.displayInput) // for maths operator
-    if (this.state.displayInput !== ""){
-      console.log("first")
+    if(this.state.evaluated){
       this.setState({
-        displayInput : ini 
-        ? this.state.displayInput + e.target.value
-        : regex 
-        ? this.state.displayInput 
-            : des 
-            ? this.state.displayInput  
-              : signs 
-              ? this.state.displayInput + "0" + e.target.value
-              : this.state.displayInput + e.target.value,
-        result: !endsWithOperator.test(this.state.displayInput) ? this.state.result : "0" + e.target.value
-        
-      })
-    } else {
-      console.log("second")
+        displayInput: '0.',
+        result: '0.',
+        evaluated: false
+      });
+    } else if (!this.state.result.includes('.')){
       this.setState({
-        displayInput: '0'  + e.target.value,
-        result: this.state.result  + e.target.value
-        
+        evaluated: false
       })
+      if (endsWithOperator.test(this.state.displayInput) || (this.state.result === '0' && this.state.displayInput === '')
+      ) {
+        this.setState({
+          result: '0.',
+          displayInput: this.state.displayInput + '0.'
+        });
+      } else {
+        this.setState({
+          result: this.state.displayInput.match(/(-?\d+\.?\d*)$/)[0] + '.',
+          displayInput: this.state.displayInput + '.'
+        });
+      }
     }
+
+    // let ini = /[\+\-\x\/]\d+$/.test(this.state.displayInput);  // checks +23 
+    // let regex = /(\d+\.\d+)$/g.test(this.state.displayInput); // checks 12.34
+    // let des = /\.$/.test(this.state.displayInput); // match decimal
+    // let signs = /[\+\-\x\/]$/.test(this.state.displayInput) // for maths operator
+    // if (this.state.displayInput !== ""){
+    //   this.setState({
+    //     displayInput : ini 
+    //     ? this.state.displayInput + e.target.value
+    //     : regex 
+    //     ? this.state.displayInput 
+    //         : des 
+    //         ? this.state.displayInput  
+    //           : signs 
+    //           ? this.state.displayInput + "0" + e.target.value
+    //           : this.state.displayInput + e.target.value,
+    //     result: !endsWithOperator.test(this.state.displayInput) ? this.state.result : "0" + e.target.value
+        
+    //   })
+    // } else {
+    //   this.setState({
+    //     displayInput: '0'  + e.target.value,
+    //     result: this.state.result  + e.target.value    
+    //   })
+    // }
   }
   handleEqualsTo(){
     let cleanData = this.state.displayInput;
     cleanData = cleanData.replace(/x/g, '*')
     let ans = /(^\W?\d+)(\W+?\d+)+/.test(cleanData)
-    // let eq = /\=$/.test(this.state.displayInput)
+
     if (ans){
-      console.log("result")
       this.setState({
         result : eval(cleanData),
         displayInput : this.state.displayInput + "=" + eval(cleanData),
         prevVal: eval(cleanData),
         evaluated: true
-        // displayInput : this.state.displayInput[this.state.displayInput.length - 1] === "=" 
-        // ? this.state.displayInput
-        // : this.state.displayInput + '=' + this.state.result
       })
     }
   }
